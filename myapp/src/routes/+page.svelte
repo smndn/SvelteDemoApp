982c28c3e8c2d29bd5638e422b86cf3b39e8bc52
Simon Dean <simon@smndn.com>
2022-12-21 23:30:50 +1100

    SetUp

---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<h1>Welcome to Simon's Demo Svelte App</h1>
+
+<script>
+	let count = 0;
+	$: doubled = count * 2;
+    $: tripled = count * 3;
+    $: console.log('the count is ' + count);
+    $: {
+        console.log('the count is ' + count);
+        alert('I SAID THE COUNT IS ' + count);
+    }
+        $: if (count >= 10) {
+        alert('count is dangerously high!');
+        count = 9;
+    }
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<button on:click={handleClick}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+
+<p>{count} doubled is {doubled}</p>
+<p>{count} tripled is {tripled}</p>
